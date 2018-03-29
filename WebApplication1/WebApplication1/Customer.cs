aac877e4b0878beee1051d1e51076d4e0da4d713
sofani <sofanib@gmail.com>
2018-03-29 10:54:59 -0400

    Begner .net core Tutorial

---
+++ WebApplication1/WebApplication1/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(10)]
+        public string Name { get; set; }
+
+    }
+}
--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
@@ -21,12 +23,21 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("name")
+                
+            );
+            services.AddLogging();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
